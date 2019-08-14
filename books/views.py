c8130ff74b7d84ab28ef6bdea177e52bf171e529
2044smile <2044smile@naver.com>
2019-08-14 22:36:25 +0900

    form 꾸미기까지 완료
    index page 꾸미기

--- books/views.py
+++ books/views.py
@@ -17,17 +17,15 @@ class BookListView(ListView):
 class BookCreateView(CreateView):
     model = Book
     success_url = reverse_lazy('list')
+    template_name = 'books/book_create.html'
     form_class = BookForm
-
-    def create(self):
-        if not self.user.is_authenticated():
-            return redirect(settings.LOGIN_URL)
 
 class BookUpdateView(UpdateView):
     model = Book
     fields = ['title','author','text','publisher','bookimage']
     # form_class = BookForm fields를 사용하거나 form_class를 사용하거나 둘 중 하나를 사용할 수 있다.
     success_url = '/list'
+    # 자동으로 _form.html 을 불러온다
 
 class BookDeleteView(DeleteView):
     model = Book
